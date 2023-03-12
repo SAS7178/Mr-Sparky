caeb1c4c0ec7b64a6d138a80f4593752672e1d78
Stephen Smith <StephenASmith7178@gmail.com>
2023-03-12 16:19:09 -0400

    creates moon geometry with wrapped texture, loop star movement and cube wrapped image

--- client/mrsparky/src/App.js
+++ client/mrsparky/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { isClickableInput } from '@testing-library/user-event/dist/utils';
 
 function App() {
 
@@ -10,49 +11,97 @@ function App() {
   const renderer = new THREE.WebGL1Renderer({
     canvas: document.querySelector('#bg'),
   });
-  const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-  const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
-  const torus = new THREE.Mesh( geometry, material)
-  renderer.setPixelRatio( window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight );
+  const geometry = new THREE.TorusGeometry(10, 3, 26, 100)
+  const material = new THREE.MeshStandardMaterial({ color: 0xDAA52 });
+  const torus = new THREE.Mesh(geometry, material)
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
   camera.position.setZ(30);
   scene.add(torus)
-  
+
+  const SASTexture = new THREE.TextureLoader().load("SAS.jpg");
+
+  // box geometry
+  const SAS = new THREE.Mesh( 
+  new THREE.BoxGeometry(6, 6, 6),
+  new THREE.MeshBasicMaterial({ map: SASTexture })
+  );
+  scene.add(SAS)
+
+  //moon
+  const moonTexture = new THREE.TextureLoader().load("moonimage.jpg");
+
+  const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(3,32,32),
+    new THREE.MeshStandardMaterial( {
+      map: moonTexture,
+    })
+  )
+  // moon.addEventListener( 'click',function(){moonScale()}, false)
+  scene.add(moon)
   //light options
   const ambientLight = new THREE.AmbientLight(0xffffff);
   const pointLight = new THREE.PointLight(0xffffff)
   const lightHelper = new THREE.PointLightHelper(pointLight)
-  
+
   //create Orbit Control object
   const controls = new OrbitControls(camera, renderer.domElement);
-  pointLight.position.set(20,20,20)
-  
+  pointLight.position.set(20, 20, 20)
+
   //call to scene
   scene.add(pointLight, ambientLight)
   scene.add(lightHelper)
+
   //call render w arguments
-  renderer.render(scene,camera);
-  
-  //animation function to rerender for movement
+  renderer.render(scene, camera);
+
+  //animation function to rerender for movement of torus
   function animate() {
     requestAnimationFrame(animate);
 
-    torus.rotation.x += 0.01;
-    torus.rotation.y += 0.005;
-    torus.rotation.z += 0.01;
+    torus.rotation.x += 0.02;
+    torus.rotation.y += 0.008;
+    torus.rotation.z += 0.02;
     controls.update();
-    renderer.render(scene,camera)
+
+
+    // Loop through all stars and reset their position if they have gone out of bounds
+    scene.children.forEach(child => {
+      if (child instanceof THREE.Mesh && child.geometry instanceof THREE.SphereGeometry) {
+        if (child.position.x > 50) {
+          child.position.x = -50;
+        }
+        if (child.position.y > 50) {
+          child.position.y = -50;
+        }
+        if (child.position.z > 50) {
+          child.position.z = -50;
+        }
+      }
+    });
+    // Loop through all stars and update their position
+    scene.children.forEach(child => {
+      if (child instanceof THREE.Mesh && child.geometry instanceof THREE.SphereGeometry) {
+        child.position.x += 0.1;
+        child.position.y += 0.1;
+        child.position.z += 0.1;
+      }
+    });
+    renderer.render(scene, camera)
   }
+
+  //function to create random stars throughout scene space
   function addstar() {
-    const geometry = new THREE.SphereGeometry(0.25,24, 24)
-    const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+    const geometry = new THREE.SphereGeometry(0.25, 14, 14)
+    const material = new THREE.MeshStandardMaterial({ color: 0xDAA520 });
     const star = new THREE.Mesh(geometry, material);
 
-    const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-    star.position.set(x,y,z);
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+    star.position.set(x, y, z);
     scene.add(star)
   }
-  Array(200).fill().forEach(addstar);
+
+  Array(300).fill().forEach(addstar);
   const spaceTexture = new THREE.TextureLoader().load('pexels.jpg');
   scene.background = spaceTexture
   animate()
